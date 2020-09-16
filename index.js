0e49c3a2d257877c0177a2e49b3dadfa9b40de67
Saniee <asamsku10@gmail.com>
2020-09-16 18:24:59 +0200

    -.- getting annoying

--- index.js
+++ index.js
@@ -19,15 +19,20 @@ const channel = client.channels.cache.find(channel => channel.id === 58663693400
 
 snooper.watcher.getPostWatcher('dankmemes')
     .on('post', function(post) {
-        var NewPostEmbed = new Discord.MessageEmbed()
-            .setAuthor(post.data.author)
-            .setTitle(post.data.title)
-            .setImage(post.data.url)
-            .setURL('https://www.reddit.com/r/factorio/')
-            .setTimestamp()
-            .setFooter('Automated Message by Discord-Redditor!')
-        client.channel.send(NewPostEmbed)
+        console.log(`New post! By ${post.data.author}`)
+        var post = post;
     })
     .on('error', console.error)  
+
+console.log(post)
+var NewPostEmbed = new Discord.MessageEmbed()
+    .setAuthor(post.data.author)
+    .setTitle(post.data.title)
+    .setImage(post.data.url)
+    .setURL('https://www.reddit.com/r/factorio/')
+    .setTimestamp()
+    .setFooter('Automated Message by Discord-Redditor!')
+channel.send(NewPostEmbed)
+
 
 client.login(process.env.TOKEN)
