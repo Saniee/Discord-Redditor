f064481e7bdbd695b91169b9ef5a55375f7f7136
Saniee <asamsku10@gmail.com>
2020-09-16 16:51:05 +0200

    still not working

--- index.js
+++ index.js
@@ -3,27 +3,26 @@ const Snooper = require('reddit-snooper')
 
 const client = new Discord.Client();
 
+snooper = new Snooper(
+    {
+        app_id: 'bdLPs7vNHCpD5w',
+        api_secret: `${process.env.api_secret}`,
+        automatic_retries: true, 
+        api_requests_per_minuite: 60
+    })
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-snooper = new Snooper(
-{
-    app_id: 'bdLPs7vNHCpD5w',
-    api_secret: `${process.env.api_secret}`,
-    automatic_retries: true, 
-    api_requests_per_minuite: 60
-})
-
 var on = new Boolean(true)
 
-while (on) {
-    snooper.watcher.getPostWatcher('r/dankmemes')
+snooper.watcher.getPostWatcher('r/dankmemes')
     .on('post', function(post) {
         console.log(`Post Posted by: ${post.data.author}`)
         console.log(post)
     })
     .on('error', console.error)  
-}
+
 
 client.login(process.env.TOKEN)
