a22586fe63e4245930c434803bf69b0d7c8c1999
Saniee <asamsku10@gmail.com>
2020-09-16 18:37:20 +0200

    why do you not work again
    I dont know man

--- index.js
+++ index.js
@@ -15,21 +15,23 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-const channel = client.channels.cache.find(channel => channel.id === 586636934005260483)
-
 snooper.watcher.getPostWatcher('dankmemes')
-    .on('post', function(post, Discord) {
-        console.log(`New post! By ${post.data.author}`)
-        console.log(post)
-        var NewPostEmbed = new Discord.MessageEmbed()
-            .setAuthor(post.data.author)
-            .setTitle(post.data.title)
-            .setImage(post.data.url)
-            .setURL('https://www.reddit.com/r/factorio/')
-            .setTimestamp()
-            .setFooter('Automated Message by Discord-Redditor!')
-        channel.send(NewPostEmbed)
+    .on('post', function(post) {
+        sendEmbed(post)
     })
     .on('error', console.error)  
+
+function sendEmbed(post) {
+    const channel = client.channels.cache.find(channel => channel.id === 586636934005260483)
+    console.log(`New post! By ${post.data.author}`)
+    var NewPostEmbed = new Discord.MessageEmbed()
+        .setAuthor(post.data.author)
+        .setTitle(post.data.title)
+        .setImage(post.data.url)
+        .setURL('https://www.reddit.com/r/factorio/')
+        .setTimestamp()
+        .setFooter('Automated Message by Discord-Redditor!')
+    channel.send(NewPostEmbed);
+}
 
 client.login(process.env.TOKEN)
