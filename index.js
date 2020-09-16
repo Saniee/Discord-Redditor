f38ba37e06cd2c2a212359a0d892c529110c4a51
Saniee <asamsku10@gmail.com>
2020-09-16 16:46:50 +0200

    a little fuck up
    forgot to change bot.user.tag to client.user.tag

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Snooper = require('reddit-snooper')
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log(`Logged in as ${bot.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 snooper = new Snooper(
