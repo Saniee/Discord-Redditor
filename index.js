4e10c5cf1e9d0db075ccf5fe705255a14cdb3932
Saniee <asamsku10@gmail.com>
2020-09-16 17:04:57 +0200

    Works
    Testing with r/dankmemes since they are very active!

--- index.js
+++ index.js
@@ -15,12 +15,18 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-var on = new Boolean(true)
+var NewPostEmbed = new Discord.MessageEmbed()
+    .setAuthor(post.data.author)
+    .setTitle(post.data.title)
+    .setImage(post.data.url)
+    .setTimestamp()
+    .setFooter('Automated Message by Discord-Redditor!')
 
-snooper.watcher.getPostWatcher('r/dankmemes')
+snooper.watcher.getPostWatcher('dankmemes')
     .on('post', function(post) {
         console.log(`Post Posted by: ${post.data.author}`)
         console.log(post)
+        message.channel.send(NewPostEmbed)
     })
     .on('error', console.error)  
 
