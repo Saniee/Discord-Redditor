6ea30b1f456df48f6e5a4f5c0e10bc1005c699c8
Saniee <asamsku10@gmail.com>
2020-09-16 18:46:14 +0200

    Redone the whole thing

--- index.js
+++ index.js
@@ -15,23 +15,23 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+var NewPostEmbed = new Discord.MessageEmbed()
+    .setAuthor(post.data.author)
+    .setTitle(post.data.title)
+    .setImage(post.data.url)
+    .setURL('https://www.reddit.com/r/factorio/')
+    .setTimestamp()
+    .setFooter('Automated Message by Discord-Redditor!')
+
 snooper.watcher.getPostWatcher('dankmemes')
     .on('post', function(post) {
         sendEmbed(post)
     })
     .on('error', console.error)  
 
-function sendEmbed(post, Discord) {
-    const channel = client.channels.cache.find(channel => channel.id === '586636934005260483')
+function sendEmbed(post, client) {
     console.log(`New post! By ${post.data.author}`)
-    var NewPostEmbed = new Discord.MessageEmbed()
-        .setAuthor(post.data.author)
-        .setTitle(post.data.title)
-        .setImage(post.data.url)
-        .setURL('https://www.reddit.com/r/factorio/')
-        .setTimestamp()
-        .setFooter('Automated Message by Discord-Redditor!')
-    channel.send(NewPostEmbed);
+    client.channels.get("586636934005260483").send(NewPostEmbed);
 }
 
 client.login(process.env.TOKEN)
