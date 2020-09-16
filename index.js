cd80b288a00d544942524c4a29de0a9cdc1a2341
Saniee <asamsku10@gmail.com>
2020-09-16 19:38:35 +0200

    Works!
    Im can happly say that it works, the reddit formating is a bit rough and not implemented. But the posts are now visible and are showing! Hurray! This was a rolecoaster of emotions... Anyway more updates coming I know it!
    
    Co-Authored-By: Gerwin Stokebrand <45660156+gstokebrand@users.noreply.github.com>

--- index.js
+++ index.js
@@ -15,22 +15,24 @@ client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-snooper.watcher.getPostWatcher('dankmemes')
+snooper.watcher.getPostWatcher('factorio')
     .on('post', function(post) {
-        sendEmbed(post)
+        sendEmbed(post, client)
     })
     .on('error', console.error)  
 
 function sendEmbed(post, client) {
     var NewPostEmbed = new Discord.MessageEmbed()
-        .setAuthor(post.data.author)
+        .setAuthor(`Author: ${post.data.author}`)
         .setTitle(post.data.title)
+        .setDescription(post.data.selftext)
         .setImage(post.data.url)
-        .setURL('https://www.reddit.com/r/factorio/')
+        .setURL(`https://www.reddit.com${post.data.permalink}`)
         .setTimestamp()
         .setFooter('Automated Message by Discord-Redditor!')
     console.log(`New post! By ${post.data.author}`)
-    client.channels.cache.get("586636934005260483").send(NewPostEmbed);
+    console.log(post)
+    client.channels.cache.get('586636934005260483').send(NewPostEmbed);
 }
 
 client.login(process.env.TOKEN)
