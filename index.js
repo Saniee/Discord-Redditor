57077432c293177157d4390c3b89a43259d324e1
Saniee <asamsku10@gmail.com>
2020-09-16 19:56:04 +0200

    New features.
    Presence + auto restart at 00:05

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ snooper = new Snooper(
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setActivity('/r/factorio', {type: 'WATCHING'})
 });
 
 snooper.watcher.getPostWatcher('factorio')
